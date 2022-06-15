ada05d340ba9e06eda70a49fbbf4b6e57f616131
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-06-15 05:54:16 -0400

    wip updates

--- src/ComicBookInventory.Domain/Entities/Character.cs
+++ src/ComicBookInventory.Domain/Entities/Character.cs
@@ -2,10 +2,16 @@
 
 namespace ComicBookInventory.Shared
 {
-    public class Character
+    public class Character : ICharacter
     {
         public int Id { get; set; }
         public string FullName { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string? SecondaryAbility { get; set; }
+        public string? Species { get; set; }
+        public string? Alias { get; set; }
+        public bool IsAlive { get; set; }
+        public string? Weapon { get; set; }
 
         // navigation properties
         [JsonIgnore]
--- src/ComicBookInventory.Domain/Interfaces/ICharacter.cs
+++ src/ComicBookInventory.Domain/Interfaces/ICharacter.cs
@@ -2,6 +2,12 @@
 {
     public interface ICharacter
     {
-        string? FullName { get; set; }
+        public string FullName { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string? SecondaryAbility { get; set; }
+        public string? Species { get; set; }
+        public string? Alias { get; set; }
+        public bool IsAlive { get; set; }
+        public string? Weapon { get; set; }
     }
 }
