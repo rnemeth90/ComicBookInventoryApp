1e4921260365b15206d2be9475e18a40cc6043e3
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-06-17 16:07:19 -0400

    unrelated updates

---
+++ src/ComicBookInventory.DataAccess.EFCore/Migrations/20220617165839_extendingModels.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComicBookInventory.DataAccess.EFCore.Migrations
+{
+    public partial class extendingModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Alias",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAlive",
+                table: "Characters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrimaryAbility",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondaryAbility",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Species",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Weapon",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Alias",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "IsAlive",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "PrimaryAbility",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "SecondaryAbility",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Species",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Weapon",
+                table: "Characters");
+        }
+    }
+}
